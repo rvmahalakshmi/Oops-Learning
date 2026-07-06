60ec8ad70defd5473ac0af8683b11a18178bc28a
venkatamahalakshmirayasam <venkatamahalakshmirayasam@gmail.com>
2026-07-06 22:07:10 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,18 @@
+ class D {
+    public static void main(String[] args) {
+        System.out.println("D class running");
+    }
+}
+
+class E {
+    public static void main(String[] args) {
+        System.out.println("E class running");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test class running");
+    }
+}
+
---
+++ student.java
@@ -0,0 +1,6 @@
+public class student{
+    public static void main(String[] args){
+        A.main(args);
+        B.main(args);
+    }
+}
